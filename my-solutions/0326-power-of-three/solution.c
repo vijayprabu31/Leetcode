1c879068136a63b8490c6e9885f57d5911f1053d
vijayprabu31 <vijayprabu012005@gmail.com>
2025-08-25 06:38:36 +0000

    [LeetCode Sync] Runtime - 1 ms (71.49%), Memory - 8.4 MB (100.00%)

--- my-solutions/0326-power-of-three/solution.c
+++ my-solutions/0326-power-of-three/solution.c
@@ -1,16 +1,12 @@
 bool isPowerOfThree(int n)
 {
-    if(n<0 || n==0)
+    if(n==1)
+    {
+        return true;
+    }
+    if(n<=0 || n%3!=0) 
     {
         return false;
     }
-    int i;
-    for(i=0;pow(3,i)<=n;i++)
-    {
-        if(pow(3,i)==n)
-        {
-            return true;
-        }
-    }  
-    return false;  
+    return isPowerOfThree(n/3);
 }
