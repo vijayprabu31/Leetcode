440eb654b4703b66e478ac4384ddd6c3f00ad4bb
vijayprabu31 <vijayprabu012005@gmail.com>
2025-09-08 06:56:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.3 MB (100.00%)

---
+++ my-solutions/0258-add-digits/solution.c
@@ -0,0 +1,12 @@
+int addDigits(int num) {
+    int sum;
+    while(num > 9){
+        sum = 0;
+        while (num>0){
+        sum = sum + num%10;
+        num = num/10;
+        }
+        num = sum;
+    }
+    return num; 
+}
