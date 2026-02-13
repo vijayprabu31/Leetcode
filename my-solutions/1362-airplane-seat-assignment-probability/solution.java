4969bb5e91b597b2650ead4a9e6de57016e4649a
vijayprabu31 <vijayprabu012005@gmail.com>
2026-02-13 07:03:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (37.50%)

---
+++ my-solutions/1362-airplane-seat-assignment-probability/solution.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public double nthPersonGetsNthSeat(int n) 
+    {
+        if(n==1)
+        {
+            return 1.00000;
+        }
+        else
+        {
+            return 0.50000;
+        }
+    }
+}
