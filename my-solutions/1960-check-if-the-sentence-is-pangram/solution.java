b3eb1286ca7ddcc9fa6d522141719f0f93011100
vijayprabu31 <vijayprabu012005@gmail.com>
2026-03-17 06:38:05 +0000

    [LeetCode Sync] Runtime - 1 ms (81.93%), Memory - 43.1 MB (17.35%)

---
+++ my-solutions/1960-check-if-the-sentence-is-pangram/solution.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public boolean checkIfPangram(String sentence) 
+    {
+        if (sentence.length() < 26) 
+            return false;
+        boolean[] seen = new boolean[26];
+        for (char c : sentence.toCharArray()) 
+        {
+            seen[c - 'a'] = true;
+        }
+        for (boolean b : seen) 
+        {
+            if (!b) 
+                return false;
+        }
+        return true;
+    }
+}
+
