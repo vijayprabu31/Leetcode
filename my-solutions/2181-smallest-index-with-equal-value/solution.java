34b6f7ab12bdae5f34012f579f511ddfff947028
vijayprabu31 <vijayprabu012005@gmail.com>
2026-06-16 09:15:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.5 MB (20.75%)

---
+++ my-solutions/2181-smallest-index-with-equal-value/solution.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int smallestEqual(int[] nums) 
+    {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(i%10==nums[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
