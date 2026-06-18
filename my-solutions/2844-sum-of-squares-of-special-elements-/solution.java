287a4d3835a7ac38340e5e3d7d51f1faf106a917
vijayprabu31 <vijayprabu012005@gmail.com>
2026-06-18 09:19:54 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46.6 MB (15.37%)

---
+++ my-solutions/2844-sum-of-squares-of-special-elements-/solution.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int sumOfSquares(int[] nums) 
+    {
+        int sum=0;
+        int n=nums.length;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                int j=nums[i-1]*nums[i-1];
+                sum+=j;
+            }
+        }
+        return sum;
+    }
+}
