f3814e92d07c2a9870a3397fd52a0966485b12d9
vijayprabu31 <vijayprabu012005@gmail.com>
2026-06-18 08:44:52 +0000

    [LeetCode Sync] Runtime - 2085 ms (53.36%), Memory - 41.9 MB (80.61%)

---
+++ my-solutions/0507-perfect-number/solution.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public boolean checkPerfectNumber(int num) 
+    {
+        int sum=0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(sum==num)
+        {
+            return true;
+        }
+        return false;
+    }
+}
