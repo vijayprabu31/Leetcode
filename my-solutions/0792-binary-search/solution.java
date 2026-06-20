5d5c106425cc9ce7f8c44dad8b2c17e02e254b4a
vijayprabu31 <vijayprabu012005@gmail.com>
2026-06-20 05:24:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.5 MB (14.32%)

---
+++ my-solutions/0792-binary-search/solution.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int left=0;
+        int right=nums.length-1;
+        int res=-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            {
+                res=mid;
+                break;
+            }
+            else if(nums[mid]<target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        return res;
+    }
+}
