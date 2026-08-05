a3fdc9f09bf30af83d9acec17c267e8e1bcc3df2
vijayprabu31 <vijayprabu012005@gmail.com>
2026-08-05 06:18:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (57.63%)

---
+++ my-solutions/1965-sum-of-digits-in-base-k/solution.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int sumBase(int n, int k) 
+    {
+        int sum=0;
+        while(n>0)
+        {
+            int r=n%k;
+            sum+=r;
+            n/=k;
+        }
+        return sum;
+    }
+}
